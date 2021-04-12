3138ec813165cece7bca1462132d4b8739536363
Marcus Ramse <marcus@ramse.se>
2021-04-12 19:11:43 +0200

    Remove all date formatting, too unstable

--- rsscli.sh
+++ rsscli.sh
@@ -6,37 +6,17 @@ TAB='	'
 
 mkdir -p "$DATA_DIR"
 
-format_date() { # input-date-format date
-	# TODO: The `date` util varies greatly depending on system.
-	#       The busybox variant can't take timezone into account when parsing
-	#       a custom date format, so an alternative solution is needed for accurate time.
-	set -e
-	if date --help 2>&1 | grep -q GNU; then
-		date -ud "$2" +'%F %T'
-	else
-		date -ud "$2" -D "$1" +'%F %T'
-	fi
-}
-format_feed_dates() { # input-date-format < feed-items
-	while read -r line; do
-		echo "$(format_date "$1" "$(echo "$line" | cut -f 1)")$TAB$(echo "$line" | cut -f 2-)" || echo "$line"
-	done
-}
-
 parse_rss() { # < data
 	set -e
-	items=$(xml sel -T -t -m //item -v pubDate -o "$TAB" -v title -o "$TAB" -v link -n 2>/dev/null)
-	echo "$items" | format_feed_dates '%a, %d %b %Y %T'
+	xml sel -T -t -m //item -v pubDate -o "$TAB" -v title -o "$TAB" -v link -n 2>/dev/null
 }
 parse_atom() { # < data
 	set -e
-	items=$(xml sel -T -N atom='http://www.w3.org/2005/Atom' -t -m //atom:entry -v atom:updated -o "$TAB" -v atom:title -o "$TAB" -v atom:link/@href -n 2>/dev/null)
-	echo "$items" | format_feed_dates '%Y-%m-%dT%H:%M:%S'
+	xml sel -T -N atom='http://www.w3.org/2005/Atom' -t -m //atom:entry -v atom:updated -o "$TAB" -v atom:title -o "$TAB" -v atom:link/@href -n 2>/dev/null
 }
 parse_rdf() { # < data
 	set -e
-	items=$(xml sel -T -N purl='http://purl.org/rss/1.0/' -t -m //purl:item -v dc:date -o "$TAB" -v purl:title -o "$TAB" -v purl:link -n 2>/dev/null)
-	echo "$items" | format_feed_dates '%Y-%m-%dT%H:%M:%S'
+	xml sel -T -N purl='http://purl.org/rss/1.0/' -t -m //purl:item -v dc:date -o "$TAB" -v purl:title -o "$TAB" -v purl:link -n 2>/dev/null
 }
 parse_feed() { # < data
 	input=$(cat -)
@@ -104,6 +84,7 @@ case $1 in
 	run)
 		# TODO: parallel
 		while read -r line; do
+			echo 1>&2 "$(echo "$line" | cut -f 1)"
 			unread=$(fetch_unread "$(echo "$line" | cut -f 2-)")
 			[ -n "$unread" ] && printf "%s\n%s\n\n" "$(echo "$line" | cut -f 1)" "$unread"
 		done < "$FEEDS_FILE"
